7c0476d4a75256692586c5d76e3d42072186860e
Amerdo Ahmed <98106747+amerdoahmed@users.noreply.github.com>
2025-02-23 15:05:04 +0000

    Update Perfect Number - Not Perfect Number.cpp

--- Perfect Number - Not Perfect Number.cpp
+++ Perfect Number - Not Perfect Number.cpp
@@ -18,15 +18,13 @@ int ReadPoitiveNumber(string Message)
 
 bool isPerfectNumber(int Number)
 {
-    int Sum = 0;
-    for (int i = 1;i < Number;i++)
-    {
-        if (Number % i == 0)
-        {
-            Sum += i;
-        }
-        return Number == Sum;
-    }
+int Sum = 0;
+for (int i = 1; i < Number; i++)
+{
+if (Number % i == 0)
+Sum += i;
+}
+return Number == Sum;
 }
 
 void PrintResults(int Number)
