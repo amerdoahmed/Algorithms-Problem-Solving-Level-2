14f6608812452979cf5aaed1b08ce2de4c659634
Amerdo Ahmed <98106747+amerdoahmed@users.noreply.github.com>
2025-04-27 16:09:13 +0100

    Add files via upload

---
+++ Count Negative Numbers in Array.cpp
@@ -0,0 +1,65 @@
+// Count Negative Numbers in Array :
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter number of elements\n";
+    cin >> arrLength;
+
+    for (int i = 0;i < arrLength;i++)
+        arr[i] = RandomNumber(-100, 100);
+
+}
+
+void PrintArray(int arr[100], int arrLength)
+{
+    for (int i = 0;i < arrLength;i++)
+        cout << arr[i] << " ";
+
+    cout << "\n";
+}
+
+int NegativeCount(int arr[100], int Length)
+{
+    int Counter = 0;
+    for (int i = 0;i < Length;i++)
+    {
+        if (arr[i] < 0)
+        {
+            Counter++;
+        }
+    }
+    return Counter;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements : \n";
+    PrintArray(arr, arrLength);
+
+    cout << "\Negative Numbers Count is : ";
+    cout << NegativeCount(arr, arrLength) << endl;
+
+    return 0;
+}
+
+
+
